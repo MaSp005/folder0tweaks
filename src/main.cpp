505954432288a574a9f708b1f9702940442fbc1d
masp005 <spelsberg.marcel@gmail.com>
2025-04-27 20:20:56 +0200

    feat: Label levels when viewing folder 0

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,64 @@
 
 using namespace geode::prelude;
 
-#include <Geode/modify/MenuLayer.hpp>
-class $modify(MyMenuLayer, MenuLayer) {
-	bool init() {
-		if (!MenuLayer::init()) {
-			return false;
+#include <Geode/modify/LevelBrowserLayer.hpp>
+class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
+
+	CCArray* getLevelNodeList() {
+		if (auto listView = this->m_list->getChildByID("list-view")) {
+			if (auto tableView = listView->getChildByType<TableView>(0)) {
+				if (auto content = tableView->getChildByType<CCContentLayer>(0)) {
+					return content->getChildren();
+				}
+				else return nullptr;
+			}
+			else return nullptr;
+		}
+		else return nullptr;
+	};
+
+	void setupLevelBrowser(CCArray* p0) {
+
+		LevelBrowserLayer::setupLevelBrowser(p0);
+		if (this->m_searchObject->m_searchType != SearchType::MyLevels) return;
+
+		CCArray* levels = getLevelNodeList();
+		if (!levels) return;
+
+		bool hideSetting = Mod::get()->getSettingValue<bool>("hideNon0");
+		log::debug("setupLevelBrowser {} {}", p0->count(), hideSetting);
+
+		for (int i = 0; i < levels->count(); i++) {
+			LevelCell* levelObj = static_cast<LevelCell*>(levels->objectAtIndex(i));
+			int lvlFolder = levelObj->m_level->m_levelFolder;
+
+			auto nameSprite = levelObj->m_mainLayer->getChildByID("level-name");
+			
+			auto sprite = CCSprite::createWithSpriteFrameName("gj_folderBtn_001.png");
+			sprite->setID("folder"_spr);
+			sprite->setAnchorPoint(CCPoint(0.0f, 0.5f));
+			sprite->setScale(0.75f);
+			sprite->setPosition(nameSprite->getPosition() + CCPoint(
+				nameSprite->getContentSize().width * nameSprite->getScaleX() + 7.0f,
+				-3.75f / nameSprite->getScaleY()
+			));
+
+			auto textSprite = CCLabelBMFont::create(
+				numToString(lvlFolder, 0).c_str(),
+				"bigFont.fnt"
+			);
+			textSprite->setAnchorPoint(CCPoint(0.5f, 0.5f));
+			textSprite->setPosition(CCPoint(
+				sprite->getContentSize().width / 2,
+				sprite->getContentSize().height / 2
+			));
+			textSprite->setScale(0.55f);
+			sprite->addChild(textSprite);
+
+			levelObj->m_mainLayer->addChild(sprite);
 		}
 
-		log::debug("Hello from my MenuLayer::init hook! This layer has {} children.", this->getChildrenCount());
+	};
 
-		auto myButton = CCMenuItemSpriteExtra::create(
-			CCSprite::createWithSpriteFrameName("GJ_likeBtn_001.png"),
-			this,
-			menu_selector(MyMenuLayer::onMyButton)
-		);
 
-		auto menu = this->getChildByID("bottom-menu");
-		menu->addChild(myButton);
-
-		myButton->setID("my-button"_spr);
-
-		menu->updateLayout();
-
-		return true;
-	}
-
-	void onMyButton(CCObject*) {
-		FLAlertLayer::create("Geode", "Hello from my custom mod!", "OK")->show();
-	}
 };
