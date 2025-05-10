28d7b754f189addae33a455e073d667acd000298
masp005 <spelsberg.marcel@gmail.com>
2025-05-11 01:34:38 +0200

    feat: Shrink and move icon when revision text is present
    + more try-catch more good :)

--- src/main.cpp
+++ src/main.cpp
@@ -24,16 +24,21 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 			this->m_searchObject->m_folder) return LevelBrowserLayer::setupLevelBrowser(p0);
 
 		if (Mod::get()->getSettingValue<bool>("hideNon0")) {
-			log::debug("Removing.... {} {}", this->m_levels->count(), this->m_itemCount);
-			for (int i = 0; i < this->m_levels->count(); i++) {
-				auto obj = this->m_levels->objectAtIndex(i);
-				auto lvl = static_cast<GJGameLevel*>(obj);
-				if (lvl->m_levelFolder) {
-					this->m_levels->removeObjectAtIndex(i);
-					i--;
+			try {
+				log::debug("Removing.... {} {}", this->m_levels->count(), this->m_itemCount);
+				for (int i = 0; i < this->m_levels->count(); i++) {
+					auto obj = this->m_levels->objectAtIndex(i);
+					auto lvl = static_cast<GJGameLevel*>(obj);
+					if (lvl->m_levelFolder) {
+						this->m_levels->removeObjectAtIndex(i);
+						i--;
+					}
 				}
+				return LevelBrowserLayer::setupLevelBrowser(p0);
 			}
-			return LevelBrowserLayer::setupLevelBrowser(p0);
+			catch (...) {
+				log::warn("Removing levels failed, trying to show folders instead.");
+			}
 		}
 
 		LevelBrowserLayer::setupLevelBrowser(p0);
@@ -44,32 +49,37 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 
 		for (int i = 0; i < levels->count(); i++) {
 			LevelCell* levelObj = static_cast<LevelCell*>(levels->objectAtIndex(i));
-			int lvlFolder = levelObj->m_level->m_levelFolder;
+			try {
+				int lvlFolder = levelObj->m_level->m_levelFolder;
 
-			auto nameSprite = levelObj->m_mainLayer->getChildByID("level-name");
+				auto nameSprite = levelObj->m_mainLayer->getChildByID("level-name");
+				auto hasRevision = levelObj->m_mainLayer->getChildByID("level-revision") != nullptr;
 
-			auto sprite = CCSprite::createWithSpriteFrameName("gj_folderBtn_001.png");
-			sprite->setID("folder"_spr);
-			sprite->setAnchorPoint(CCPoint(0.0f, 0.5f));
-			sprite->setScale(0.75f);
-			sprite->setPosition(nameSprite->getPosition() + CCPoint(
-				nameSprite->getContentSize().width * nameSprite->getScaleX() + 7.0f,
-				-3.75f * nameSprite->getScaleY()
-			));
+				auto folderSprite = CCSprite::createWithSpriteFrameName("gj_folderBtn_001.png");
+				folderSprite->setID("folder"_spr);
+				folderSprite->setAnchorPoint(CCPoint(0.f, 0.5f));
+				folderSprite->setScale(hasRevision ? 0.5f : 0.75f);
+				folderSprite->setPosition(nameSprite->getPosition() + CCPoint(
+					nameSprite->getContentSize().width * nameSprite->getScaleX() + 7.f,
+					-3.75f * nameSprite->getScaleY() + (hasRevision ? 10.f : 0.f)
+				));
 
-			auto textSprite = CCLabelBMFont::create(
-				numToString(lvlFolder, 0).c_str(),
-				"bigFont.fnt"
-			);
-			textSprite->setAnchorPoint(CCPoint(0.5f, 0.5f));
-			textSprite->setPosition(CCPoint(
-				sprite->getContentSize().width / 2,
-				sprite->getContentSize().height / 2
-			));
-			textSprite->setScale(0.55f);
-			sprite->addChild(textSprite);
+				auto textSprite = CCLabelBMFont::create(
+					numToString(lvlFolder, 0).c_str(),
+					"bigFont.fnt"
+				);
+				textSprite->setAnchorPoint(CCPoint(0.5f, 0.5f));
+				textSprite->setPosition(CCPoint(
+					folderSprite->getContentSize().width / 2,
+					folderSprite->getContentSize().height / 2
+				));
+				textSprite->setScale(0.55f);
+				folderSprite->addChild(textSprite);
 
-			levelObj->m_mainLayer->addChild(sprite);
+				levelObj->m_mainLayer->addChild(folderSprite);
+			} catch (...) {
+				log::warn("Folder indicator on level {} failed.", levelObj->m_level->m_levelName);
+			}
 		}
 
 	};
