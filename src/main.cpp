ea399fe11f03549a1ad18778ad9d9aa2e14dd12f
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-05-11 18:07:13 -0400

    hmm i hope i didn't break anything

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,17 @@
-#include <Geode/Geode.hpp>
+#include <Geode/modify/LevelBrowserLayer.hpp>
 
 using namespace geode::prelude;
 
-#include <Geode/modify/LevelBrowserLayer.hpp>
 class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 
-	CCContentLayer* getLevelNodeList() {
-		if (auto listView = this->m_list->getChildByID("list-view")) {
-			if (auto tableView = listView->getChildByType<TableView>(0)) {
-				if (auto content = tableView->getChildByType<CCContentLayer>(0)) {
+	CCArray* getCellArray() {
+		if (auto listView = this->m_list->m_listView) {
+			if (auto tableView = listView->m_tableView) {
+				if (auto content = tableView->m_cellArray) {
 					return content;
-				}
-				else return nullptr;
-			}
-			else return nullptr;
-		}
-		else return nullptr;
+				} else return nullptr;
+			} else return nullptr;
+		} else return nullptr;
 	};
 
 	void setupLevelBrowser(CCArray* p0) {
@@ -24,7 +20,7 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 			this->m_searchObject->m_folder) return LevelBrowserLayer::setupLevelBrowser(p0);
 
 		if (Mod::get()->getSettingValue<bool>("hideNon0")) {
-			try {
+			if (typeinfo_cast<GJGameLevel*>(this->m_levels->objectAtIndex(0))) {
 				log::debug("Removing.... {} {}", this->m_levels->count(), this->m_itemCount);
 				for (int i = 0; i < this->m_levels->count(); i++) {
 					auto obj = this->m_levels->objectAtIndex(i);
@@ -36,50 +32,45 @@ class $modify(MyLevelBrowserLayer, LevelBrowserLayer) {
 				}
 				return LevelBrowserLayer::setupLevelBrowser(p0);
 			}
-			catch (...) {
+			else {
 				log::warn("Removing levels failed, trying to show folders instead.");
 			}
 		}
 
 		LevelBrowserLayer::setupLevelBrowser(p0);
-
-		CCContentLayer* levelLayer = getLevelNodeList();
-		if (!levelLayer) return;
-		CCArray* levels = levelLayer->getChildren();
+		CCArray* levels = getCellArray();
+		if (!levels) return;
 
 		for (int i = 0; i < levels->count(); i++) {
 			LevelCell* levelObj = static_cast<LevelCell*>(levels->objectAtIndex(i));
-			try {
-				int lvlFolder = levelObj->m_level->m_levelFolder;
+			int lvlFolder = levelObj->m_level->m_levelFolder;
 
-				auto nameSprite = levelObj->m_mainLayer->getChildByID("level-name");
-				auto hasRevision = levelObj->m_mainLayer->getChildByID("level-revision") != nullptr;
-
-				auto folderSprite = CCSprite::createWithSpriteFrameName("gj_folderBtn_001.png");
-				folderSprite->setID("folder"_spr);
-				folderSprite->setAnchorPoint(CCPoint(0.f, 0.5f));
-				folderSprite->setScale(hasRevision ? 0.5f : 0.75f);
-				folderSprite->setPosition(nameSprite->getPosition() + CCPoint(
-					nameSprite->getContentSize().width * nameSprite->getScaleX() + 7.f,
-					-3.75f * nameSprite->getScaleY() + (hasRevision ? 10.f : 0.f)
-				));
-
-				auto textSprite = CCLabelBMFont::create(
-					numToString(lvlFolder, 0).c_str(),
-					"bigFont.fnt"
-				);
-				textSprite->setAnchorPoint(CCPoint(0.5f, 0.5f));
-				textSprite->setPosition(CCPoint(
-					folderSprite->getContentSize().width / 2,
-					folderSprite->getContentSize().height / 2
-				));
-				textSprite->setScale(0.55f);
-				folderSprite->addChild(textSprite);
-
-				levelObj->m_mainLayer->addChild(folderSprite);
-			} catch (...) {
+			auto nameSprite = levelObj->m_mainLayer->getChildByID("level-name");
+			if (!nameSprite) {
 				log::warn("Folder indicator on level {} failed.", levelObj->m_level->m_levelName);
+				continue;
 			}
+			auto hasRevision = levelObj->m_mainLayer->getChildByID("level-revision") != nullptr;
+
+			auto folderSprite = CCSprite::createWithSpriteFrameName("gj_folderBtn_001.png");
+			folderSprite->setID("folder"_spr);
+			folderSprite->setAnchorPoint(CCPoint(0.f, 0.5f));
+			folderSprite->setScale(hasRevision ? 0.5f : 0.75f);
+			folderSprite->setPosition(nameSprite->getPosition() + CCPoint(
+				nameSprite->getContentSize().width * nameSprite->getScaleX() + 7.f,
+				-3.75f * nameSprite->getScaleY() + (hasRevision ? 10.f : 0.f)
+			));
+
+			auto textSprite = CCLabelBMFont::create(
+				numToString(lvlFolder, 0).c_str(),
+				"bigFont.fnt"
+			);
+			textSprite->setAnchorPoint({0.5f, 0.5f});
+			textSprite->setPosition(folderSprite->getContentSize() / 2.f);
+			textSprite->setScale(0.55f);
+			folderSprite->addChild(textSprite);
+
+			levelObj->m_mainLayer->addChild(folderSprite);
 		}
 
 	};
